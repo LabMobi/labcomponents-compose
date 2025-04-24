3a2aab687bd3b6b9bef907454683e556bd1056a6
Harri Kirik <harri.kirik@lab.mobi>
2025-04-24 17:11:29 +0300

    MW-45: Add progress on the indicator

---
+++ lib/src/main/java/mobi/lab/components/compose/widget/progress/LabIndeterminateProgressDefaults.kt
@@ -0,0 +1,8 @@
+package mobi.lab.components.compose.widget.progress
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+public object LabIndeterminateProgressDefaults {
+    public val buttonProgressSize: Dp = 24.dp
+}
